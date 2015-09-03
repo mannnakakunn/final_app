a95a8afee3e12f816415809172981df60e99bbbf
Mannnakakunn <mannnakakunn@gmail.com>
2015-09-04 06:29:04 +0900

    Finish static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,13 @@
 class StaticPagesController < ApplicationController
   def home
   end
+  # defはfunctionみたいなもの。
+  # Modelとのやりとりをするときにどんなデータが欲しい、というような記述を行う
+ 
 
   def help
+  end
+
+  def about
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 FinalApp::Application.routes.draw do
   get "static_pages/home"
+  # HTTPのGETRequestが来た時にどんなコントローラー、ビューが反応するのかを決める。
   get "static_pages/help"
+  get "static_pages/about"
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
